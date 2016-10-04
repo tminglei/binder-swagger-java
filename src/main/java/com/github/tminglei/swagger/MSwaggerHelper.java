1e36f7ccee1f037b7969cef9c982e124dc8328f3
Minglei Tu <tmlneu@gmail.com>
2016-10-04 12:09:36 +0800

    param example support

--- src/main/java/com/github/tminglei/swagger/MSwaggerHelper.java
+++ src/main/java/com/github/tminglei/swagger/MSwaggerHelper.java
@@ -1,7 +1,5 @@
 package com.github.tminglei.swagger;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.github.tminglei.bind.Framework;
 import com.github.tminglei.bind.spi.Constraint;
 import com.github.tminglei.bind.spi.ExtraConstraint;
@@ -27,7 +25,6 @@ import static com.github.tminglei.bind.OptionsOps.*;
  * Helper class to build swagger elements from `com.github.tminglei.bind.Framework.Mapping`
  */
 public class MSwaggerHelper {
-    private ObjectWriter objectWriter = new ObjectMapper().writer();
 
     public List<Parameter> mToParameters(String name, Framework.Mapping<?> mapping) {
         if ("body".equalsIgnoreCase( attach(mapping).in() )) {
@@ -95,7 +92,7 @@ public class MSwaggerHelper {
                 .format(format(mapping))
                 .required(required(mapping))
                 .description(attach(mapping).desc())
-                .example(example(mapping))
+                .example(exampleString(mapping))
                 .items(items(mapping))
                 ._enum(enums(mapping));
 
@@ -354,13 +351,9 @@ public class MSwaggerHelper {
         return attach(mapping).format();
     }
 
-    protected String example(Framework.Mapping<?> mapping) {
+    protected String exampleString(Framework.Mapping<?> mapping) {
         Object example = attach(mapping).example();
-        try {
-            return example == null ? null : objectWriter.writeValueAsString(example);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return example == null ? null : example.toString();
     }
 
     protected boolean required(Framework.Mapping<?> mapping) {
